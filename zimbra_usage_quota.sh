f5751b026ed6e4cf3f6dee8ecda8117ee69cf252
kargozia <129495527+kargozia@users.noreply.github.com>
2023-05-18 14:50:59 +0300

    Update zimbra_usage_quota.sh

--- zimbra_usage_quota.sh
+++ zimbra_usage_quota.sh
@@ -10,6 +10,6 @@ account_info=$(zmaccts)
 echo "$quota_info" | while read -r line; do
     email=$(echo "$line" | awk '{print $1}')
     quota=$(echo "$line" | awk '{printf "%s %.0fMB %.0fMB\n", $1, ($3 / (1024 * 1024)), ($2 / (1024 * 1024))}')
-    info=$(echo "$account_info" | awk -v email="$email" '$1 == email {print $2, $3, $4}')
+    info=$(echo "$account_info" | awk -v email="$email" '$1 == email {print $2, $3, $4, $5, $6}')
     echo "$quota $info"
 done
